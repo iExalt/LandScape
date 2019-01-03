93a0f2bd34bcafd1ebed4f5ca6276ebe35c809ef
Clement Liaw <cklliaw@uwaterloo.ca>
2019-01-02 23:19:45 -0500

    Added deletion functionality

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LandScape
 Bare Metal as Code. No relation to Terraform.
+
+# Usage
+1. Populate file with desired configuration (LandScape uses main.ls by default)
+    - Multiple hosts can be created
+2. Run python3 landscape.py create (-f FILE)
+3. Run python3 landscape.py delete (-f FILE) to delete configuration
--- landscape.py
+++ landscape.py
@@ -28,9 +28,9 @@ create.add_argument("-s", "--state", help="state file")
 # update.add_argument("-f", "--file", help="state file")
 #
 delete = subparser.add_parser("delete", help="Destroy current Packet architecture")
-delete.add_argument("-f", "--file", help="state file")
+delete.add_argument("-s", "--state", help="state file")
 
-# test = subparser.add_parser("delete", help="Self test")
+# test = subparser.add_parser("delete", help="Self test - setup host, run commands and check output")
 # test.add_argument("-f", "--file", help="state file")
 
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
@@ -59,71 +59,100 @@ if args.invoked_command == "create":
     logging.info(current_devices_response)
     current_devices = json.loads(current_devices_response.text)
 
-
     try:
         with open(args.state or "main.ls") as f:
-            state = yaml.load(f)
-            logging.info("Requested state: %s", state)
-            # Add ssh key if set
-            if "ssh_key" in state[0]:
-                ssh_key_create_response = requests.post('https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/ssh-keys', \
-                headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")}, json={"key" : state[0]["ssh_key"], "label" : "Created by landscape" })
-                logging.info(ssh_key_create_response)
+            state_list = yaml.load(f)
+            logging.info("Requested state: %s", state_list)
 
-                ssh_key_search_response = requests.get('https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/ssh-keys', \
-                headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")})
-                logging.info(ssh_key_search_response)
-                ssh_key_search = json.loads(ssh_key_search_response.text)
+            save_states = []
 
-                # ssh key reverse lookup
-                # for key_data in ssh_key_search["ssh_keys"]:
-                #     if key_data["key"] == state[0]["ssh_key"]:
-                #         state[0]["project_ssh_keys"] = key_data["id"]
-                #         break
+            for state in state_list:
+                # Add ssh key if set
+                if "ssh_key" in state:
+                    ssh_key_create_response = requests.post('https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/ssh-keys', \
+                    headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")}, json={"key" : state["ssh_key"], "label" : "Created by landscape" })
+                    logging.info(ssh_key_create_response)
+
+                    ssh_key_search_response = requests.get('https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/ssh-keys', \
+                    headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")})
+                    logging.info(ssh_key_search_response)
+                    ssh_key_search = json.loads(ssh_key_search_response.text)
+
+                    # Doesn't work for some reason
+
+                    # ssh key reverse lookup
+                    # for key_data in ssh_key_search["ssh_keys"]:
+                    #     if key_data["key"] == state[0]["ssh_key"]:
+                    #         state[0]["project_ssh_keys"] = key_data["id"]
+                    #         break
 
 
 
-            # Warn if hostname in use
-            for device in current_devices["devices"]:
-                if device["hostname"] == state[0]["hostname"]:
-                    logging.warn("Hostname in use!")
+                # Warn if hostname in use
+                for device in current_devices["devices"]:
+                    if device["hostname"] == state["hostname"]:
+                        logging.warn("Hostname in use!")
 
-            # Make new device
-            new_device_json_stripped = state[0]
-            try:
-                del new_device_json_stripped["ssh_key"]
-            except KeyError:
-                pass
-            print(new_device_json_stripped)
+                # Make new device
+                new_device_json_stripped = state
+                try:
+                    del new_device_json_stripped["ssh_key"]
+                except KeyError:
+                    pass
+                logging.info(new_device_json_stripped)
 
-            # Actually create new device
-            create_device_response = requests.post("https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/devices", \
-            headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")}, json=new_device_json_stripped)
-            create_device_response = create_device_response.text
-            logging.info(create_device_response)
+                # Actually create new device
+                create_device_response = requests.post("https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/devices", \
+                headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")}, json=new_device_json_stripped)
+                create_device = create_device_response.text
+                logging.info(create_device_response)
+
+                create_device_object = json.loads(create_device)
+                save_state = {  "id" : create_device_object["id"],
+                                "hostname"  : create_device_object["hostname"],
+                                "facility"  : create_device_object["facility"]["code"],
+                                "plan"      : create_device_object["plan"]["slug"],
+                                "operating_system"      : create_device_object["operating_system"]["slug"]
+                                }
+
+                save_states.append(save_state)
 
             try:
                 with open((args.state or "main")+".lsstate", "w+") as current_state:
-                    wanted_keys = ["id", "hostname", "facility", "plan", "operating_system"]
-                    create_device_object = json.loads(create_device_response)
-                    save_state = {  "id" : create_device_object["id"],
-                                    "hostname"  : create_device_object["hostname"],
-                                    "facility"  : create_device_object["facility"]["code"],
-                                    "plan"      : create_device_object["plan"]["slug"],
-                                    "operating_system"      : create_device_object["operating_system"]["slug"]
-                                    }
-
-                    yaml.dump(save_state, current_state)
+                    yaml.dump(save_states, current_state)
+                    logging.info("Current state: %s", save_state)
                     current_state.close()
 
             except IOError as error:
-                print("Could not open lsstate file!")
+                logging.error("Could not open lsstate file!")
+                sys.exit(1)
 
     except IOError as error:
-        print("Error: file \"{0}\" does not exist!".format(args.state or "main.ls"))
+        logging.error("Error: file \"{0}\" does not exist!".format(args.state or "main.ls"))
+        sys.exit(1)
 
 elif args.invoked_command == "delete":
-    pass
+
+    # Delete all IDs specified
+
+    try:
+        with open((args.state or "main")+".lsstate") as f:
+            state_list = yaml.load(f)
+            for state in state_list:
+                delete_device_response = requests.delete("https://api.packet.net/devices/{0}".format(state["id"]), \
+                headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")})
+                delete_device = delete_device_response.text
+                if 200 <= delete_device_response.status_code <= 299:
+                    logging.info("Device {0} successfully deleted".format(state["hostname"]))
+                logging.info(delete_device_response)
+            f.close()
+
+        os.remove((args.state or "main")+".lsstate")
+
+    except IOError as error:
+        print("Error: No .lsstate file found!")
+        sys.exit(1)
+
 
 
 
