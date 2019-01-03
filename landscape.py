8a843d848e8ae9002b097e9df3b35547627ea2ff
Clement Liaw <cklliaw@uwaterloo.ca>
2019-01-02 20:05:42 -0500

    Add new host/ssh key works

--- landscape.py
+++ landscape.py
@@ -1,7 +1,15 @@
 from ruamel.yaml import YAML
+import json
 import requests
 import argparse
 import sys
+import os
+import logging
+
+# Change working directory to landscape.py location always
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 yaml=YAML(typ='safe')
 parser = argparse.ArgumentParser()
@@ -26,20 +34,64 @@ create.add_argument("-s", "--state", help="state file")
 
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 
-
-
+# Print out help if no arguments are given
+# Why isn't this default python behaviour???
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
     sys.exit(1)
 args = parser.parse_args()
-# print(args.file)
-# print(args.selected)
-print(args)
-# print(parser.parse_args(["all"]))
+
+# Set up logging
+log_level = "DEBUG"
+
+if args.verbose == True:
+    log_level = "DEBUG"
+else:
+    log_level = "WARN"
+
+logging.basicConfig(level = log_level, format="%(asctime)s %(levelname)-8s %(message)s", datefmt = "%Y-%m-%d %H:%M:%S")
 
 if args.invoked_command == "create":
     # Get current devices
+    current_devices_response = requests.get("https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/devices", \
+    headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")})
+    logging.info(current_devices_response)
+    current_devices = current_devices_response.text
 
-    # Make new device
+
+    try:
+        with open(args.state or "main.ls") as f:
+            state = yaml.load(f)
+            logging.info("Requested state: %s", state)
+            # Add ssh key if set
+            if "ssh_key" in state[0]:
+                ssh_key_response = requests.post('https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/ssh-keys', \
+                headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")}, json={"key" : state[0]["ssh_key"], "label" : "Created by landscape" })
+                logging.info(ssh_key_response)
+
+            # Warn if hostname in use
+
+            # Make new device
+            new_device_json_stripped = state[0]
+            try:
+                del new_device_json_stripped["ssh_key"]
+            except KeyError:
+                pass
+            print(new_device_json_stripped)
+            create_device_response = requests.post("https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/devices", \
+            headers={"X-Auth-Token": os.environ.get("LANDSCAPE_API_KEY")}, json=new_device_json_stripped)
+            logging.info(create_device_response.text)
+
+
+
+    except IOError as error:
+        print("Error: file \"{0}\" does not exist!".format(args.state or "main.ls"))
+
+
+
+    # ssh_key_response = requests.post('https://api.packet.net/project/{id}/devices'.format())
+
+    #
+
 
     # Return created device
